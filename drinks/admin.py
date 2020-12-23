af4fb0e5b0747ca621a9743ca02c791a58dceb09
NadhivaFahriani <nadhiivaa@gmail.com>
2020-12-23 22:48:50 +0700

    I added some codes in admin.py, added init.py in controllers folder, added some codes in urls.py

--- drinks/admin.py
+++ drinks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from drinks.models import splash, ice, size, sugar, temperature, topping
 
 # Register your models here.
+admin.site.register(splash.Splash)
+admin.site.register(ice.Ice)
+admin.site.register(size.Size)
+admin.site.register(sugar.Sugar)
+admin.site.register(temperature.Temperature)
+admin.site.register(topping.Topping)
---
+++ drinks/controllers/__init__.py
@@ -0,0 +1,6 @@
+from . import splash_controller
+from . import ice_controller
+from . import size_controller
+from . import sugar_controller
+from . import temperature_controller
+from . import topping_controller
