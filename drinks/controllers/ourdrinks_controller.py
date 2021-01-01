40990dfbd7b5ea01647149bfc51ad6a7d8cce7d0
NadhivaFahriani <nadhiivaa@gmail.com>
2021-01-01 22:51:56 +0700

    added pagination in ourdrinks but still didnt work

--- drinks/controllers/ourdrinks_controller.py
+++ drinks/controllers/ourdrinks_controller.py
@@ -1,10 +1,15 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from drinks.models.ourdrinks import ourDrinks
 
 
 def index(request):
     ourdrinks = ourDrinks.objects.all()
-    context = {
-        'ourdrinks': ourdrinks,
+    paginator = Paginator(ourdrinks, 4)  
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    data = {
+        'page_obj': page_obj,
     }
-    return render(request, 'ourdrinks/index.html', context=context)
+    return render(request, 'ourdrinks/index.html', data)
