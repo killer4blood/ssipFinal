c6cdc7915bcb2da550b6534939e92eea2887352a
feli <grandline.fl@gmail.com>
2021-01-02 11:29:39 +0700

    added paginator in ourdrinks. Works fine

--- drinks/controllers/ourdrinks_controller.py
+++ drinks/controllers/ourdrinks_controller.py
@@ -1,10 +1,16 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from drinks.models.ourdrinks import ourDrinks
 
 
 def index(request):
     ourdrinks = ourDrinks.objects.all()
+    paginator = Paginator(ourdrinks, 4)
+    
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
     context = {
-        'ourdrinks': ourdrinks,
+        'page_obj': page_obj,
     }
     return render(request, 'ourdrinks/index.html', context=context)
