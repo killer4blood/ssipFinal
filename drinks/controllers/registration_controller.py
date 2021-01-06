acf7ef32192b88de62cee69880ed1c42b7dd2a99
feli <grandline.fl@gmail.com>
2021-01-06 21:42:06 +0700

    added the login.html and also made logout button better

--- drinks/controllers/registration_controller.py
+++ drinks/controllers/registration_controller.py
@@ -8,26 +8,26 @@ from django.conf import settings
 
 def index(request):
     msg = ''
-    if request.method == 'POST':  # POST, we get all fields from registration page
+    if request.method == 'POST':
         req = request.POST.dict()
         username = req['username']
         password = req['password']
-        email = req['email']  # this is our target email
-        try:  # we try to find existing user first, if exists show error
+        email = req['email']
+        try:
             user = User.objects.get(username=username)
             msg = 'Username or E-Mail is already Registered'
-        except User.DoesNotExist:  # user not found, then register, send email
-            user = User.objects.create_user(username, email, password)  # register user
-            user.save()  # save to db
-            msg = ''  # empty message, no error, then send mail using configured EMAIL_HOST_USER
+        except User.DoesNotExist:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            msg = ''
             send_mail(
-                'Registration Successful',
-                'You are now an official member of SPLASH DRINK CORNER!',
+                'Splash: Registration Successful',
+                'You are now an official member of Splash Drink Corner! You can now order custom drinks.',
                 settings.EMAIL_HOST_USER,
                 [email],
                 fail_silently=True,
             )
-        return HttpResponseRedirect('/accounts/login/')  # show login page if successful
+        return HttpResponseRedirect('/accounts/login/')
     data = {
         'user_exists_error': msg,
     }
